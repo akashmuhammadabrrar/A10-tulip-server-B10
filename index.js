714cfbebdf3ed5595ae16469a0885a215f0b83f3
AkashMAbrrar <madborakash48@gmail.com>
2024-12-09 18:37:57 +0600

    this commit for fixing the campaign routes

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(express.json());
+app.use(cors());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c4n3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,12 +25,13 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // create the data cluster
     const campaignCollection = client.db("campaignDB").collection("campaign");
     const donateUserCollection = client
       .db("campaignDB")
       .collection("userDonation");
+
     //  read for single one
     app.get("/details/:id", async (req, res) => {
       const id = req.params.id;
@@ -37,10 +42,11 @@ async function run() {
     app.post("/myDonate", async (req, res) => {
       try {
         const donate = req.body;
+        console.log(req.body);
         const result = await donateUserCollection.insertOne(donate);
         res.send(result);
       } catch (error) {
-        console.log(error, "error donate user");
+        // console.log(error, "error donate user");
       }
     });
     // Read data for showing on client side
@@ -62,7 +68,6 @@ async function run() {
       const result = await query.toArray();
       res.send(result);
     });
-    // running campaign for 6 items
 
     // update campaign
     app.put("/campaigns/:id", async (req, res) => {
@@ -96,10 +101,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -107,14 +112,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-// middleware
-app.use(express.json());
-app.use(cors());
-
 app.get("/", (req, res) => {
   res.send("hello form home route campaigns");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port:${port}`);
+  // console.log(`Server is running on port:${port}`);
 });
